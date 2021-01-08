c2c22313ad09dbbdce46407c7f572f0923a475a1
carseven <carles.serra@hotmail.es>
2021-01-08 22:00:56 +0100

    Añadir filtro canny y comentarios funciones prewitt, sobel

--- percepcion/filtro-prewitt-sobel-canny.py
+++ percepcion/filtro-prewitt-sobel-canny.py
@@ -47,24 +47,49 @@ sobel_y = np.array([[[1.0, 0.0, -1.0],
                      [2.0, 0.0, -2.0],
                      [1.0, 0.0, -1.0]]])
 
+canny_k = np.array([[-1.0, -1.0, -1.0],
+                    [-1.0, 8.0, -1.0],
+                    [-1.0, -1.0, -1.0]])
+
+
+def filtro_canny(img, canny_k):
+    img = img.astype('float') / 255.0
+    return sc.convolve(img[:, :, 0], canny_k)
+
 
 def filtro_media_cero(img, k_x, k_y, rgb=False):
+
+    # Normalizamos la imagen a [0.0, 1.0]
     img = img.astype('float') / 255.0
+
+    # Convolucionamos el kernel k_x y k_y con la iamgen
+    # g_x: filtrado de los bordes en direccion eje X
+    # g_y: filtrado de los bordes en direccion eje Y
     if rgb:
         g_x = sc.convolve(img, k_x)
         g_y = sc.convolve(img, k_y)
     else:
         g_x = sc.convolve(img[:, :, 0], k_x[:, :, 0])
         g_y = sc.convolve(img[:, :, 0], k_y[:, :, 0])
+
+    # Devolvemos la combinacion de g_x y g_y -> sqrt(gx^2 + gy^2)
     return ((((g_x ** 2) + (g_y ** 2)) ** 1/2))
 
 
+# Mostrar resultados filtrado
 plt.figure(1, figsize=(7, 7))
 
-plt.subplot(211)
+plt.subplot(311)
+plt.title('PREWITT')
 plt.imshow(filtro_media_cero(img, prewitt_x, prewitt_y), cmap='gray')
 
-plt.subplot(212)
+plt.subplot(312)
+plt.title('SOBEL')
 plt.imshow(filtro_media_cero(img, sobel_x, sobel_y), cmap='gray')
 
+plt.subplot(313)
+plt.title('CANNY')
+plt.imshow(filtro_canny(img, canny_k), cmap='gray')
+
+plt.tight_layout()
 plt.show()
