8a62c3ac0422123fb8d3111ff323113de62df44f
carseven <carles.serra@hotmail.es>
2021-01-09 16:36:13 +0100

    Modificar filtro en gray, no se utilizaba el kernel adecuadamente

--- percepcion/filtro-prewitt-sobel-canny.py
+++ percepcion/filtro-prewitt-sobel-canny.py
@@ -47,37 +47,41 @@ sobel_y = np.array([[[1.0, 0.0, -1.0],
                      [2.0, 0.0, -2.0],
                      [1.0, 0.0, -1.0]]])
 
-canny_k = np.array([[-1.0, -1.0, -1.0],
-                    [-1.0, 8.0, -1.0],
-                    [-1.0, -1.0, -1.0]])
+canny_k = np.array([[[-1.0, -1.0, -1.0],
+                     [-1.0, 8.0, -1.0],
+                     [-1.0, -1.0, -1.0]],
+                    [[-1.0, -1.0, -1.0],
+                     [-1.0, 8.0, -1.0],
+                     [-1.0, -1.0, -1.0]],
+                    [[-1.0, -1.0, -1.0],
+                     [-1.0, 8.0, -1.0],
+                     [-1.0, -1.0, -1.0]],
+                    [[-1.0, -1.0, -1.0],
+                     [-1.0, 8.0, -1.0],
+                     [-1.0, -1.0, -1.0]]])
 
 
-def filtro_canny(img, canny_k):
+def filtro_canny(img, canny_k, x=8.0):
     img = img.astype('float') / 255.0
-    return sc.convolve(img[:, :, 0], canny_k)
+    canny_k[0, 1, 1] = float(x)
+    return sc.convolve(img[:, :, 0], canny_k[0, :, :])
 
 
-def filtro_media_cero(img, k_x, k_y, rgb=False):
+def filtro_media_cero(img, k_x, k_y):
 
     # Normalizamos la imagen a [0.0, 1.0]
     img = img.astype('float') / 255.0
 
     # Convolucionamos el kernel k_x y k_y con la iamgen
-    # g_x: filtrado de los bordes en direccion eje X
-    # g_y: filtrado de los bordes en direccion eje Y
-    if rgb:
-        g_x = sc.convolve(img, k_x)
-        g_y = sc.convolve(img, k_y)
-    else:
-        g_x = sc.convolve(img[:, :, 0], k_x[:, :, 0])
-        g_y = sc.convolve(img[:, :, 0], k_y[:, :, 0])
+    g_x = sc.convolve(img[:, :, 0], k_x[0, :, :])
+    g_y = sc.convolve(img[:, :, 0], k_y[0, :, :])
 
     # Devolvemos la combinacion de g_x y g_y -> sqrt(gx^2 + gy^2)
     return ((((g_x ** 2) + (g_y ** 2)) ** 1/2))
 
 
 # Mostrar resultados filtrado
-plt.figure(1, figsize=(7, 7))
+plt.figure(1, figsize=(4, 7))
 
 plt.subplot(311)
 plt.title('PREWITT')
